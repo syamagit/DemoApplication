4616447f5de3a487e6600528b083448b1a793173
Sharvani <sharvanikarrepu@gmail.com>
2022-12-15 13:32:58 +0530

    added Model details

---
+++ Astronaut.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  MockProject
+//
+//  Created by sypatra on 15/12/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable {
+    var name : String
+    var agency: String
+    var image: String
+    var  random: Int
+    var wikipedia: String
+    var launches: String
+    var  status: String
+    var id: String
+    
+}
