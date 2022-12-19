8deb3cb1657d078fe3189c075815ec49f2cf71c8
Sharvani <sharvanikarrepu@gmail.com>
2022-12-19 14:33:34 +0530

    some changes

--- UserDataListController.swift
+++ UserDataListController.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 import UIKit
-class UserDataListController: UIViewController{
+class UserDataListController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
     var viewModel: UserDataViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,10 +17,43 @@ class UserDataListController: UIViewController{
         viewModel?.getUserDataList(completion: { result in
             switch result{
             case .success:
-                print("api data retrieved successfully")
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             case .failure:
                 print("check API call ")
             }
         })
+        setup()
     }
+    func setup() {
+        let nib = UINib(nibName: "UserDataViewDemoCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "userDataViewDemoCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.viewModel?.numberOfRows() ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "userDataViewDemoCell") as? UserDataViewDemoCell {
+
+            guard let userdata = viewModel?.userDataList?[indexPath.row] else {
+                return UITableViewCell()
+            }
+
+          
+
+            cell.configureWith(userdata: userdata)
+            return cell
+        }
+        return UITableViewCell()
+       
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        self.viewModel?.numberOfSections() ?? 1
+    }
+    
 }
